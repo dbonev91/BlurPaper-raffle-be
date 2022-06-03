fde1fd17b3876b7ddca25422adeea3ad919be780
Dimitar Bonev <dimitar.bonev@adesso.bg>
2022-06-04 01:33:13 +0300

    Adopted to shared methods in the SM project

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,26 +4,12 @@ import https from "https";
 import fs from "fs";
 import { RANGE_END_PROP, RANGE_START_PROP } from "./shared/constants";
 import crypto from "crypto";
-import { config } from 'dotenv';
+import { PaperConfiguration } from '../../paper-node-configuration/src/app';
 
-const path: string | undefined = `./environment/${process.env.NODE_ENV === 'production' ? '.env.prod' : '.env'}`;
-
-config({ path });
-
-// TODO: export as generic function in common repo for all the microservices: https://dbpaper.atlassian.net/browse/SM-1
-// const MAIN_CORS_URL: string = `${process.env.CORS_PROTOCOL}://${process.env.CORS_DOMAIN}`;
-// const origin: string[] = process.env.CORS_PORTS ? process.env.CORS_PORTS.split(' ').map((port: string) => {
-//   return `${MAIN_CORS_URL}:${port.trim()}`;
-// }) : [];
-
-// origin.push('https://raffle.blurpaper.com');
-// origin.push('https://www.raffle.blurpaper.com');
-// origin.push(MAIN_CORS_URL);
-
+const paperConfiguration: PaperConfiguration<https.Server | express.Application> = new PaperConfiguration(process.env.NODE_ENV === 'production');
 const app: express.Application = express();
 app.disable("x-powered-by");
-const corsOptions: cors.CorsOptions = { origin: /^(https:\/\/)([a-zA-Z0-9\-]+)?(?!\/)(?!\?).?(blurpaper.com):?([0-9]+)?/ };
-app.use(cors(corsOptions));
+app.use(cors(paperConfiguration.getCorsOrigin()));
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
@@ -55,26 +41,4 @@ app.get(
   }
 );
 
-let server: https.Server | express.Application = app;
-
-// TODO: export as generic function in common repo for all the microservices: https://dbpaper.atlassian.net/browse/SM-1
-if (process.env.SSL_KEY && process.env.SSL_CERT) {
-  try {
-    server = https.createServer(
-      {
-        key: fs.readFileSync(process.env.SSL_KEY),
-        cert: fs.readFileSync(process.env.SSL_CERT),
-        rejectUnauthorized: false,
-      },
-      app
-    );
-  } catch (error) {
-    console.log('ERROR:');
-    console.log('Could not start with SSL connection');
-    console.log(error);
-  }
-}
-
-server.listen(process.env.PORT, () => {
-  console.log(`Paper raffle app is listening on port ${process.env.PORT}!!!!`);
-});
+paperConfiguration.startNodeServer(app, Number(process.env.PORT), process.env.APP_NAME || '', process.env.PRIVATE_KEY || '', process.env.FULL_CHAIN || '');
